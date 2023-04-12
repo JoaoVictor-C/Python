e5bf163c1455482d4a8838f33f53822ba9b9cf18
João Victor Corrêa da Silva <fffjv2006@gmail.com>
2023-04-11 22:26:41 -0300

    Update desafio063.py

--- Desafios/Mundo 2/desafio063.py
+++ Desafios/Mundo 2/desafio063.py
@@ -1,25 +1,34 @@
 print('===== DESAFIO 63 =====')
 n = int(input('Digite quantos valores você deseja ver da sequência de fibonacci:  '))
 p = 0
-fib1 = 0
-fib2 = 1
+res1 = 1
+res2 = 1
+res3 = 2
 while p <= n-1:
    if p == 0:
       print(0)
-   p += 1
-   res = fib1 + fib2
-   print(res)
-   fib2 = 
-   fib1 = res - fib2
-
+      p += 1
+   if p-n < 1:
+      print(res1)
+      res1 = res2 + res3
+      p += 1
+   if p-n < 0:
+      print(res2)
+      res2 = res1 + res3
+      p += 1
+   if p-n < 0:
+      print(res3)
+      res3 = res2 + res1
+      p += 1
+      
 # 0 +  0    = 0
-# 1º   2º   = 1       
+# 1 +  0    = 1       
 # 0 +  1    = 1        
 # 1 +  1    = 2       
-# 2 +  1    = 3        
-# 3 +  2    = 5        
-# 5 +  3    = 8        
+# 1 +  2    = 3        
+# 2 +  3    = 5        
+# 3 +  5    = 8        
 # 5 +  8    = 13       
-# 13 + 8    = 21       
-# 21 + 13   = 34      
-# 34 + 21   = 55
+# 8 +  13   = 21       
+# 13 + 21   = 34      
+# 21 + 34   = 55
