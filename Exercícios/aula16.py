cc5b28717bc69852462cd029e388e3d5f93598da
João Victor Corrêa da Silva <fffjv2006@gmail.com>
2023-06-08 15:33:31 -0300

    Aula 16

---
+++ Exercícios/aula16.py
@@ -0,0 +1,13 @@
+lanche = ('Hamburger', 'Suco', 'Pizza', 'Pudim')
+
+for c in range(0, len(lanche)):
+   print(f'Eu vou comer {lanche[c]}')
+   
+print('-'*20)
+
+for c in lanche:
+   print(f'Eu vou comer {c}')
+
+print('-'*20)
+
+print('Fim!')
