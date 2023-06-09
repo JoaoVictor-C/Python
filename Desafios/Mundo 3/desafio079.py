77843de4cdec972a4daa33325beadf7aa61eb99d
João Victor Corrêa da Silva <fffjv2006@gmail.com>
2023-06-09 15:52:01 -0300

    desafio 79
    desafio 79

---
+++ Desafios/Mundo 3/desafio079.py
@@ -0,0 +1,31 @@
+n = []
+
+teste = 'ÇÇ'
+
+while True:
+    for c in n:
+        if n.count(c) > 1:
+            n.remove(c)
+            
+    if teste == 'ÇÇ':
+        teste = 'ÇÇ'
+        
+    elif teste == 'S':
+        teste = 'S'
+        
+    elif teste == 'N':
+        break
+    
+    else:
+        teste = input('Valor inserido inválido!\nDeseja continuar? [S] [N]\n> ').upper()
+    
+    if teste == 'ÇÇ':
+        n.append(int(input('\nDigite um número:\n> ')))
+        
+    if teste != 'ÇÇ': 
+        n.append(int(input('\nDigite um número:\n> ')))
+        
+    teste = input('Deseja continuar? [S] [N]\n> ').upper()
+
+
+print(sorted(n))
