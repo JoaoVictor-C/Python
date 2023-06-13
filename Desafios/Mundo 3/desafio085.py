2a5ca0bbae84aef8ab0427f63d12a7d55c428055
João Victor Corrêa da Silva <fffjv2006@gmail.com>
2023-06-13 20:53:40 -0300

    Update desafio085.py

--- Desafios/Mundo 3/desafio085.py
+++ Desafios/Mundo 3/desafio085.py
@@ -1,20 +1,19 @@
-par = []
-impar = []
+num = [[], []]
 
 for c in range(0, 7):
     inpt = int(input('Digite um valor:\n> '))
     if inpt % 2 == 0:
-        par.append(inpt)
+        num[0].append(inpt)
     elif inpt % 2 == 1:
-        impar.append(inpt)
+        num[1].append(inpt)
 
 
-par.sort()
-impar.sort()
+num[0].sort()
+num[1].sort()
 
 print(f'Os valores pares digitados foram:', end=' ')
-for e in par:
+for e in num[0]:
     print(e, end=' ')
 print(f'\nOs valores ímpares digitados foram:', end=' ')
-for d in impar:
+for d in num[1]:
     print(d, end=' ')
